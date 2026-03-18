652ff939ac3d67340bffda0b59937eae4437d2df
Safan <AbdallahSafan05@gmail.com>
2026-03-18 02:59:10 +0200

    finished: pipeline-stage

--- source/common/material/pipeline-state.cpp
+++ source/common/material/pipeline-state.cpp
@@ -3,6 +3,36 @@
 
 namespace our {
 
+    void PipelineState::setup() const {
+        // Set the OpenGL state according to the values stored in this structure
+        // For example, if faceCulling.enabled is true, you should call glEnable(GL_CULL_FACE), otherwise, you should call glDisable(GL_CULL_FACE)
+        if(faceCulling.enabled){
+            glEnable(GL_CULL_FACE);
+            glCullFace(faceCulling.culledFace);
+            glFrontFace(faceCulling.frontFace);
+        } else {
+            glDisable(GL_CULL_FACE);
+        }
+
+        if(depthTesting.enabled){
+            glEnable(GL_DEPTH_TEST);
+            glDepthFunc(depthTesting.function);
+        } else {
+            glDisable(GL_DEPTH_TEST);
+        }
+
+        if(blending.enabled){
+            glEnable(GL_BLEND);
+            glBlendEquation(blending.equation);
+            glBlendFunc(blending.sourceFactor, blending.destinationFactor);
+            glBlendColor(blending.constantColor.r, blending.constantColor.g, blending.constantColor.b, blending.constantColor.a);
+        } else {
+            glDisable(GL_BLEND);
+        }
+
+        glColorMask(colorMask.r, colorMask.g, colorMask.b, colorMask.a);
+        glDepthMask(depthMask);
+    }
     // Given a json object, this function deserializes a PipelineState structure
     void PipelineState::deserialize(const nlohmann::json& data){
         // If the given json data does not represent a json object, return
--- source/common/material/pipeline-state.hpp
+++ source/common/material/pipeline-state.hpp
@@ -40,9 +40,7 @@ namespace our {
 
         // This function should set the OpenGL options to the values specified by this structure
         // For example, if faceCulling.enabled is true, you should call glEnable(GL_CULL_FACE), otherwise, you should call glDisable(GL_CULL_FACE)
-        void setup() const {
-            //TODO: (Req 4) Write this function
-        }
+        void setup() const;
 
         // Given a json object, this function deserializes a PipelineState structure
         void deserialize(const nlohmann::json& data);
