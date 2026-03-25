9353e7efcea9cbd76e0c74347a639f7596c36d7a
Esraa <149703255+Esraa-Hassan0@users.noreply.github.com>
2026-03-25 23:13:13 +0200

    Engine/entities (#8)
    * entity.hpp
    
    * feat: implement entities
    
    ---------
    
    Co-authored-by: Safan <AbdallahSafan05@gmail.com>

--- source/common/ecs/entity.cpp
+++ source/common/ecs/entity.cpp
@@ -4,25 +4,46 @@
 
 #include <glm/gtx/euler_angles.hpp>
 
-namespace our {
+namespace our
+{
 
     // This function returns the transformation matrix from the entity's local space to the world space
     // Remember that you can get the transformation matrix from this entity to its parent from "localTransform"
     // To get the local to world matrix, you need to combine this entities matrix with its parent's matrix and
     // its parent's parent's matrix and so on till you reach the root.
-    glm::mat4 Entity::getLocalToWorldMatrix() const {
-        //TODO: (Req 8) Write this function
-        return glm::mat4(1.0f);
+    glm::mat4 Entity::getLocalToWorldMatrix() const
+    {
+        // TODO: (Req 8) Write this function
+        // Start with this entity's own local transform matrix
+        // This converts from local space → parent's local space
+        glm::mat4 localToWorld = localTransform.toMat4();
+
+        // Walk up the parent chain until we reach the root
+        const Entity *curr = parent;
+        while (curr != nullptr)
+        {
+            // Left-multiply by the parent's transform so that:
+            // World = Root × ... × Grandparent × Parent × Local
+            // This ensures parent transforms are applied AFTER child transforms,
+            localToWorld = curr->localTransform.toMat4() * localToWorld; // column major 
+            curr = curr->parent; // Move one level up the hierarchy
+        }
+        return localToWorld;
     }
 
     // Deserializes the entity data and components from a json object
-    void Entity::deserialize(const nlohmann::json& data){
-        if(!data.is_object()) return;
+    void Entity::deserialize(const nlohmann::json &data)
+    {
+        if (!data.is_object())
+            return;
         name = data.value("name", name);
         localTransform.deserialize(data);
-        if(data.contains("components")){
-            if(const auto& components = data["components"]; components.is_array()){
-                for(auto& component: components){
+        if (data.contains("components"))
+        {
+            if (const auto &components = data["components"]; components.is_array())
+            {
+                for (auto &component : components)
+                {
                     deserializeComponent(component, this);
                 }
             }
--- source/common/ecs/entity.hpp
+++ source/common/ecs/entity.hpp
@@ -35,7 +35,10 @@ namespace our {
             static_assert(std::is_base_of<Component, T>::value, "T must inherit from Component");
             //TODO: (Req 8) Create an component of type T, set its "owner" to be this entity, then push it into the component's list
             // Don't forget to return a pointer to the new component
-            return nullptr;
+            T* newComponent = new T();
+            newComponent->owner = this;
+            components.push_back(newComponent);
+            return newComponent;
         }
 
         // This template method searhes for a component of type T and returns a pointer to it
@@ -44,6 +47,10 @@ namespace our {
         T* getComponent(){
             //TODO: (Req 8) Go through the components list and find the first component that can be dynamically cast to "T*".
             // Return the component you found, or return null of nothing was found.
+                for(auto component : components){
+                    if(auto castedComponent = dynamic_cast<T*>(component))
+                        return castedComponent;
+                }
             return nullptr;
         }
 
@@ -63,6 +70,13 @@ namespace our {
         void deleteComponent(){
             //TODO: (Req 8) Go through the components list and find the first component that can be dynamically cast to "T*".
             // If found, delete the found component and remove it from the components list
+            for (auto it = components.begin(); it != components.end(); ++it){
+                if(dynamic_cast<T*>(*it)){
+                    delete *it;
+                    components.erase(it);
+                    break;
+                }
+            }
         }
 
         // This template method searhes for a component of type T and deletes it
@@ -80,11 +94,21 @@ namespace our {
         void deleteComponent(T const* component){
             //TODO: (Req 8) Go through the components list and find the given component "component".
             // If found, delete the found component and remove it from the components list
+            for (auto it = components.begin(); it != components.end(); ++it){
+                if(*it == component){
+                    delete *it;
+                    components.erase(it);
+                    break;
+                }
+            }
         }
 
         // Since the entity owns its components, they should be deleted alongside the entity
         ~Entity(){
             //TODO: (Req 8) Delete all the components in "components".
+            for(auto component : components)
+                delete component;
+            components.clear();
         }
 
         // Entities should not be copyable
