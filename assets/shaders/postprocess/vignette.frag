f7cb7c56e4b91270d3e934fdf8a1a65b0fe69bdf
MoYasser165 <roed15006@gmail.com>
2026-04-02 18:05:03 +0200

    feat: implement forward renderer, sky rendering, and postprocessing

--- assets/shaders/postprocess/vignette.frag
+++ assets/shaders/postprocess/vignette.frag
@@ -18,5 +18,7 @@ void main(){
     // Hint: remember that the NDC space ranges from -1 to 1
     // while the texture coordinate space ranges from 0 to 1
     // We have the pixel's texture coordinate, how can we compute its location in the NDC space?
-    frag_color = texture(tex, tex_coord);    
+    vec2 ndc = tex_coord * 2.0 - 1.0;
+    float l2 = dot(ndc, ndc);
+    frag_color = texture(tex, tex_coord) / (1.0 + l2);    
 }
--- source/common/texture/texture-utils.cpp
+++ source/common/texture/texture-utils.cpp
@@ -9,7 +9,9 @@ our::Texture2D* our::texture_utils::empty(GLenum format, glm::ivec2 size){
     our::Texture2D* texture = new our::Texture2D();
     //TODO: (Req 11) Finish this function to create an empty texture with the given size and format
     texture->bind();
-    glTextureSubImage2D(texture->getOpenGLName(), 0, 0, 0, size.x, size.y, format, GL_UNSIGNED_BYTE, nullptr);
+    GLenum base_format = (format == GL_DEPTH_COMPONENT24) ? GL_DEPTH_COMPONENT : GL_RGBA;
+    GLenum type = (format == GL_DEPTH_COMPONENT24) ? GL_UNSIGNED_INT : GL_UNSIGNED_BYTE;
+    glTexImage2D(GL_TEXTURE_2D, 0, format, size.x, size.y, 0, base_format, type, nullptr);
     texture->unbind();
     return texture;
 }
